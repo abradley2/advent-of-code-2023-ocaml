61df13bc6cd570d2f6e5ef2c8693c2a55206a462
Tony Bradley <tonybradley@tonys-mbp.mynetworksettings.com>
2023-12-05 06:50:30 -0500

    stack overflow yay

--- day_04/main.ml
+++ day_04/main.ml
@@ -42,23 +42,51 @@ let input_parser =
 
 module IntSet = Set.Make (Int)
 
+type card_result = {match_count: int; score: int}
+
 let process_card (Card (_, win_nums)) my_nums =
   let win_set = IntSet.of_list win_nums and my_set = IntSet.of_list my_nums in
-  let intersect = IntSet.inter win_set my_set |> IntSet.to_list |> List.length in
-  if intersect = 0 then 0 else pow 2 (intersect - 1)
+  let match_count = IntSet.inter win_set my_set |> IntSet.to_list |> List.length in
+  {match_count; score= (if match_count = 0 then 0 else pow 2 (match_count - 1))}
 
 let process_input_pt_1 parsed_input =
   let rec process_input_pt_1 parsed_input ~results =
     match parsed_input with
-    | [] -> results
+    | [] -> List.rev results
     | (card, hand) :: next -> process_input_pt_1 next ~results:(process_card card hand :: results)
   in
-  process_input_pt_1 parsed_input ~results:[] |> List.fold_left ( + ) 0
+  process_input_pt_1 parsed_input ~results:[]
+
+let rec process_card_result ~idx ~indexed_card_results card_result =
+  let my_result = card_result.match_count in
+  let next_result =
+    Array.sub indexed_card_results idx (idx + card_result.match_count + 1)
+    |> Array.map (process_card_result ~idx ~indexed_card_results)
+    |> Array.fold_left ( + ) 0
+  in
+  my_result + next_result
+
+let process_input_pt_2 (card_results : card_result list) =
+  let indexed_card_results = Array.of_list card_results in
+  let rec process_input_pt_2 card_results ~idx ~count =
+    match card_results with
+    | [] -> count
+    | card_result :: next ->
+        process_input_pt_2 next ~idx:(idx + 1)
+          ~count:(count + process_card_result ~indexed_card_results card_result ~idx)
+  in
+  process_input_pt_2 card_results ~idx:0 ~count:0
 
 let () =
   let input = Stdio.In_channel.read_all "day_04/input.txt" and part = get_part () in
   match (part, Bark.run input_parser input) with
-  | 1, Ok parsed_input -> Printf.printf "Part One: %d\n" (process_input_pt_1 parsed_input)
-  | 2, Ok _ -> print_endline "Part 2"
+  | 1, Ok parsed_input ->
+      let results = process_input_pt_1 parsed_input in
+      let count = results |> List.map (fun v -> v.score) |> List.fold_left ( + ) 0 in
+      Printf.printf "Part One: %d\n" count
+  | 2, Ok parsed_input ->
+      let card_results = process_input_pt_1 parsed_input in
+      let count = process_input_pt_2 card_results in
+      Printf.printf "Part One: %d\n" count
   | _, Ok _ -> print_endline "Invalid part"
   | _, Error dead_ends -> print_endline (dead_ends_to_string dead_ends)
