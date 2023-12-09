f98eec9ac875dada27e828c80e9612de4f5a1378
Tony Bradley <tonybradley@tonys-mbp.mynetworksettings.com>
2023-12-09 12:51:20 -0500

    unrecurse

--- day_05/main.ml
+++ day_05/main.ml
@@ -10,7 +10,7 @@ module Range = struct
   type t = dest * src * len
 
   let in_src_range sut ((Dest dest_val, Src src_val, Len len) : t) =
-    if sut >= src_val && sut < src_val + len then Some (Base.List.range dest_val len) else None
+    if sut >= src_val && sut < src_val + len then Some (dest_val + abs (src_val - sut)) else None
 
   let make dest src len : t = (dest, src, len)
 end
@@ -22,25 +22,34 @@ module Range_map = struct
 
   type dst_key = Dst_key of string
 
-  module Src_map = Map.Make (struct
+  module M = Map.Make (struct
     type t = src_key
 
     let compare (Src_key a) (Src_key b) = compare a b
   end)
 
-  type t = (dst_key * Range.t list) Src_map.t
+  type t = (dst_key * Range.t list) M.t
+
+  (* let make l : t = M.of_list l *)
+
+  let add k v (t : t) : t = M.add k v t
+
+  let find k (t : t) = M.find k t
+
+  (* let find_opt k (t : t) = M.find_opt k t *)
+
+  let empty : t = M.empty
 end
 
 module Pair_set = Set.Make (struct
-  type t = string * int
+  type t = Range_map.src_key * int
 
-  let compare (src_key1, src_val1) (src_key2, src_val2) =
+  let compare (Range_map.Src_key src_key1, src_val1) (Range_map.Src_key src_key2, src_val2) =
     compare (src_key1 ^ string_of_int src_val1) (src_key2 ^ string_of_int src_val2)
 end)
 
 module Int_set = Set.Make (Int)
 module String_set = Set.Make (String)
-module String_map = Map.Make (String)
 
 let seeds_parser =
   let open Bark in
@@ -99,52 +108,51 @@ let input_parser =
                     ranges_parser |> map (fun ranges -> Loop ((map_to, ranges) :: groups)) )
            ; endd (Problem "Expecting EOF") |> map (fun _ -> Done (List.rev groups)) ] )
 
-let run_through_ranges src_key cur_val input (_, (ranges : Range.t list)) =
+let run_through_ranges cur_val (Range_map.Dst_key dest_key) (ranges : Range.t list) =
+  print_endline (string_of_int cur_val) ;
+  print_endline dest_key ;
   let src_matches =
-    List.map
-      (fun range -> Range.in_src_range cur_val range |> Option.value ~default:[cur_val])
-      ranges
-    |> List.flatten
+    List.map (fun range -> Range.in_src_range cur_val range |> Option.value ~default:cur_val) ranges
   in
-  let next = String_map.find_opt src_key input in
-  match next with
-  | None -> Done src_matches
-  | Some (next_dest, next_ranges) -> Loop (next_dest, next_ranges, src_matches)
+  match dest_key with
+  | "location" -> Done src_matches
+  | _ -> Loop (src_matches |> List.map (fun src_match -> (Range_map.Src_key dest_key, src_match)))
 
-let run_pairs pairs input =
+let run_pairs pairs (range_map : Range_map.t) =
   let rec run_pairs pairs ~results =
     match pairs with
     | [] -> results
     | (src_key, pair_val) :: next -> (
-        Printf.printf "%s, %d\n" src_key pair_val ;
-        let next_result =
-          run_through_ranges src_key pair_val input (String_map.find src_key input)
-        in
+        let dst_key, ranges = Range_map.find src_key range_map in
+        let next_result = run_through_ranges pair_val dst_key ranges in
         match next_result with
         | Done next_result -> run_pairs next ~results:(List.append results next_result)
-        | Loop (next_src_key, _, next_vals) ->
-            let next_pairs = List.map (fun next_val -> (next_src_key, next_val)) next_vals in
+        | Loop next_pairs ->
             let deduped = Pair_set.of_list (List.append next next_pairs) |> Pair_set.to_list in
-            run_pairs deduped ~results )
+            run_pairs (List.append next deduped) ~results )
   in
   run_pairs pairs ~results:[]
 
 let format_input (input : ((string * string) * Range.t list) list) =
+  let open Range_map in
   let rec format_input input ~formatted =
     match input with
     | [] -> formatted
     | ((src_val, dest_val), range) :: next ->
         Printf.printf "%s -> %s\n" src_val dest_val ;
-        format_input next ~formatted:(String_map.add src_val (dest_val, range) formatted)
+        format_input next ~formatted:(add (Src_key src_val) (Dst_key dest_val, range) formatted)
   in
-  format_input input ~formatted:String_map.empty
+  format_input input ~formatted:empty
 
 let () =
   let input = Stdio.In_channel.read_all "day_05/input.txt" in
   match Bark.run input_parser input with
   | Ok (seeds, range_map_list) ->
-      let formatted = format_input range_map_list in
-      let results = run_pairs (List.map (fun pair_val -> ("seed", pair_val)) seeds) formatted in
-      let smallest = List.fold_left (fun acc cur -> min acc cur) (List.hd results) results in
-      print_endline (string_of_int smallest)
+      print_endline "formatting input" ;
+      let range_map = format_input range_map_list in
+      print_endline "got range map" ;
+      let result =
+        run_pairs (List.map (fun seed -> (Range_map.Src_key "seed", seed)) seeds) range_map
+      in
+      print_endline (String.concat "\n" (List.map string_of_int result))
   | Error dead_ends -> print_endline (dead_ends_to_string dead_ends)
